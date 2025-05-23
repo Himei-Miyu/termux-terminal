3d4e01551d81a80a69753de70888bb13c44b05cc
himei-miyu <miyu@himei.city>
2025-05-23 07:08:43 +0700

    update foramt sh editor and and diff-check file

---
+++ diff-check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CONF_PATH=(
+"service/mpd/run"
+"starship.toml"
+"ssh/sshd_config.d"
+"micro/settings.json"
+"mpd/mpd.conf"
+"ncmpcpp/config"
+"htop/htoprc"
+)
+
+for i in ${CONF_PATH[@]}; do
+  name=$(basename "$i")
+  [[ $i =~ ^service ]] && {
+    diff $i $PREFIX/var/$i
+    test $? -gt 0 && echo "[SERVICE-DETECT] $i has changed"
+    continue
+  }
+  [[ $name =~ \.d$ ]] && {
+    diff config/$i $PREFIX/etc/$i
+    test $? -gt 0 && echo "[DIR-DETECT] $i has changed"
+    continue
+  }
+  [[ $i == "starship.toml" ]] && {
+    diff config/starship/starship.toml ~/.config/$i
+    test $? -gt 0 && echo "[CONFIG-DETECT] $i has changed"
+    continue
+  }
+  diff config/$i ~/.config/$i &> /dev/null;
+  test $? -gt 0 && echo "[CONFIG-DETECT] $i has changed"
+done
+
+echo Different check completed
